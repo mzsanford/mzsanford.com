b424d4bd17b7ecade5b285cf4f400982e04ac8a3
Matt Sanford <matt@mzsanford.com>
2011-11-27 21:35:22 -0800

    New styles. New profiles

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 
 var fs = require('fs');
 
+var requestHost = function(req) {
+	return req.header('Host').split(/:/)[0];
+};
+
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Matt Sanford' })
+exports.index = function(req, res) {
+  if (requestHost(req).match(/^blog/)) {
+    res.render('blog', { layout: false });
+  } else {
+    res.render('index', { title: 'Matt Sanford' });
+  }
 };
 
 /*
